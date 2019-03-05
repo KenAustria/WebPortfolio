7ac4167009ce2953d943b6d6f256f6378091ec23
Ken Austria <kenaustriadev@gmail.com>
2019-03-05 15:20:22 -0800

    CSS Defaults and initial Portfolio and Profile component

---
+++ src/home/Portfolio/Portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Portfolio.module.scss';
+import Profile from '../Profile/Profile';
+
+const Portfolio = () => {
+	return (
+		<div className={classes.Portfolio}>
+			<Profile />
+		</div>
+	);
+};
+
+export default Portfolio;
---
+++ src/home/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Profile.module.scss";
+
+const Profile = () => {
+	return (
+		<div>
+			Profile
+		</div>
+	);
+};
+
+export default Profile;
