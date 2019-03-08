ca72927f6df0db826166e9983e8acbf217b6bbb8
Ken Austria <kenaustriadev@gmail.com>
2019-03-08 00:30:47 -0800

    Summary component and styles

---
+++ src/components/Summary/Summary.js
@@ -0,0 +1,24 @@
+import React from "react";
+import classes from "./Summary.module.scss";
+
+const Summary = () => {
+  return (
+    <div className={classes.profileSection}>
+      <h1 className={classes.profileTitle}>ken austria</h1>
+      <p>
+        Hello! I'm Ken, a front-end engineer based in San Francisco, USA.
+        Positioned to utilize solid design and conceptual skills to meet and
+        exceed organizational targets. In a deeply immersive agile development
+        setting, I thrive on quick iterations, transparent communication during
+        stand-up meetings, and quality pairing when necessary. I've gained
+        hands-on experience in identifying areas of optimization and injected my
+        own opinions on how things can be improved beyond specifications. Feel
+        free to get in touch!
+      </p>
+    </div>
+  );
+};
+
+export default Summary;
+
+
