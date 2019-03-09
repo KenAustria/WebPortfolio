91ef4558bf99c3e85405c32cc3d25f22ee5b2097
Ken Austria <kenaustriadev@gmail.com>
2019-03-09 12:27:14 -0800

    Cleanup and Minor Styling Edits

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import Portfolio from './Components/Portfolio/Portfolio';
+import React, { Component } from "react";
+import Portfolio from "./Components/Portfolio/Portfolio";
 
 class App extends Component {
   render() {
     return (
-			<div>
-				<Portfolio />
-			</div>
+      <div>
+        <Portfolio />
+      </div>
     );
   }
 }
